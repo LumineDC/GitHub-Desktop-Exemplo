1f28652153cf5a5e69b19d35d467b3f14c98b21d
Larissa Catrinck <larissadiascatrinck@gmail.com>
2026-04-13 09:05:06 -0300

    Revert "Hello World"
    This reverts commit 7d04b5e4035e777438a1116497df77648179bcd5.

--- GitHub_Desktop_Exemplo/src/main/java/br/com/senac/github_desktop_exemplo/GitHub_Desktop_Exemplo.java
+++ GitHub_Desktop_Exemplo/src/main/java/br/com/senac/github_desktop_exemplo/GitHub_Desktop_Exemplo.java
@@ -1,9 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
 
 package br.com.senac.github_desktop_exemplo;
 
+/**
+ *
+ * @author user
+ */
 public class GitHub_Desktop_Exemplo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
     }
 }
